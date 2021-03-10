2207ccf63130b72a8f35f9aaf10e8646f543176e
Francois <francoischarih@sce.carleton.ca>
2021-03-10 10:17:41 -0500

    Add link to Gareth's paper pre-print.

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,11 +5,11 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/fcharih/fcharih.github.io/src/pages/404.js"))),
-  "component---src-pages-blog-js": hot(preferDefault(require("/Users/fcharih/fcharih.github.io/src/pages/blog.js"))),
-  "component---src-pages-cv-js": hot(preferDefault(require("/Users/fcharih/fcharih.github.io/src/pages/cv.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/fcharih/fcharih.github.io/src/pages/index.js"))),
-  "component---src-pages-music-js": hot(preferDefault(require("/Users/fcharih/fcharih.github.io/src/pages/music.js"))),
-  "component---src-pages-research-js": hot(preferDefault(require("/Users/fcharih/fcharih.github.io/src/pages/research.js")))
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/fcharih/my-website/src/pages/404.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("/Users/fcharih/my-website/src/pages/blog.js"))),
+  "component---src-pages-cv-js": hot(preferDefault(require("/Users/fcharih/my-website/src/pages/cv.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/fcharih/my-website/src/pages/index.js"))),
+  "component---src-pages-music-js": hot(preferDefault(require("/Users/fcharih/my-website/src/pages/music.js"))),
+  "component---src-pages-research-js": hot(preferDefault(require("/Users/fcharih/my-website/src/pages/research.js")))
 }
 
